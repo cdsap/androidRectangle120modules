985e5e60f7a5fb47b6a523909f3522c27ffa50ac
Inaki Villar <inaki.seri@gmail.com>
2025-12-15 19:40:36 -0400

    Adding shrinking

--- build-logic/convention/src/main/kotlin/com/logic/CompositeBuildPluginAndroidApp.kt
+++ build-logic/convention/src/main/kotlin/com/logic/CompositeBuildPluginAndroidApp.kt
@@ -40,6 +40,7 @@ class CompositeBuildPluginAndroidApp : Plugin<Project> {
                 buildTypes {
                     getByName("release") {
                         isMinifyEnabled = true
+                        isShrinkResources = true
                         proguardFiles(
                             getDefaultProguardFile("proguard-android-optimize.txt"),
                             "proguard-rules.pro"
